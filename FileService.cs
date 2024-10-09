943d05be74b193b008c868950e8a252b0116bab5
NayeAzofeifa <nayeriazofeifa3003@gmail.com>
2024-10-09 06:24:01 -0600

    The “Incoming” folder is added with its respective txt file to be parsed and the “FileService” class is added for the file services as well as the function to read the file.

---
+++ FileService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileParserToCSV
+{
+    public class FileService
+    {
+        public List<string> readFile(string path)
+        {
+            List<string> fileLines = File.ReadAllLines(path).ToList();
+            return fileLines;
+        }
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+using System.Runtime.CompilerServices;
+
+namespace FileParserToCSV
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var fileService = new FileService();
+            string inputFilePath = @"C:\\Users\\Margot Porras\\source\\repos\\FileParserToCSV\\Incoming\\InputData.txt";
+            List<string> inputFileLines = fileService.readFile(inputFilePath);
+            foreach (string line in inputFileLines)
+            {
+                Console.WriteLine(line);
+            }
+            Console.ReadLine();
+        }
+    }
+}
